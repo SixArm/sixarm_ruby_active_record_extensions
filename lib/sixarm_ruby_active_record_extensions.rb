513c8992fa0431f0eabd6be4f074f1f8dc814da0
SixArm <sixarm@sixarm.com>
2010-10-17 19:40:03 -0700

    first

---
+++ lib/sixarm_ruby_active_record_extensions.rb
@@ -0,0 +1,21 @@
+=begin rdoc
+
+= SixArm Ruby Gem: ActiveRecord Extensions for Ruby On Rails
+
+Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
+Copyright:: Copyright (c) 2006-2010 Joel Parker Henderson
+License:: CreativeCommons License, Non-commercial Share Alike
+License:: LGPL, GNU Lesser General Public License
+
+RubyOnRails ActiveRecord base class extensions
+
+=end
+
+
+require 'activerecord'
+
+class ActiveRecord::Base
+
+  named_scope :read, :readonly => true
+
+end
---
+++ test/sixarm_ruby_active_record_extensions_test.rb
@@ -0,0 +1,11 @@
+require 'test/unit'
+require 'sixarm_ruby_active_record_extensions'
+
+class Testing < Test::Unit::TestCase
+
+ def test_nop
+  #nop
+ end
+
+end
+
